b96f971994df6b5e9558163f41330bd7b3a65a03
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-07-08 00:42:34 +0100

    code refactored

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,7 +18,7 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
+    navItems.forEach((item) => item.classList.add("show"));
 
     //Set menu state
     showMenu = true;
@@ -27,7 +27,7 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show"));
+    navItems.forEach((item) => item.classList.remove("show"));
 
     //Set menu state
     showMenu = false;
